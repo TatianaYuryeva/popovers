49751be3f55cfad087d48848286b67c73e22c5d6
Tatiana Yuryeva <yuryevat@list.ru>
2023-06-13 16:59:32 +0200

    webpack.config updated

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  transformIgnorePatterns: [`/node_modules/(?!${popovers})`],
+  transformIgnorePatterns: [`/node_modules/(?!${puppeteer})`],
   devServer: {
     port: 9000,
   },
