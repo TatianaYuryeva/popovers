a80f2b549cf60d43026aee1b10819fa29bdedbc5
Tatiana Yuryeva <yuryevat@list.ru>
2023-06-14 17:50:50 +0200

    appveyor error fixed

--- e2e/popovers.test.js
+++ e2e/popovers.test.js
@@ -5,56 +5,29 @@
 import { Widget } from "../src/js/widget"
 
 test('Widget should render on page start', () => {
-    const container = document.querySelectorAll('.container')
-    const widget = new Widget(container)
+      const container = document.querySelectorAll('.container')
+      const widget = new Widget(container)
 
-    widget.bindToDOM()
+      widget.bindToDOM()
 
-    expect(container.innerHTML).toEqual(Widget.markup)
+      expect(container.innerHTML).toEqual(Widget.markup)
     });
 
-// import puppeteer from 'puppeteer';
+    test('Widget buttons should show popover on click and close popover on next click', () => {
+      const container = document.querySelectorAll('.container')
+      const widget = new Widget(container)
 
-// describe('Popover widget', () => {
-//   let browser;
-//   let page;
+      widget.bindToDOM()
 
-//   beforeEach(async () => {
-//     browser = await puppeteer.launch({
-//       headless: true,
-//       slowMo: 100,
-//       devtools: true,
-//       args: ["--no-sandbox"]
-//     });
-
-//     page = await browser.newPage();
-//   }, 60000);
-
-//   test('Widget should render on page start', async () => {
-//     await page.goto('http://localhost:9000');
-
-//     await page.waitForSelector('.widget');
-//   }, 120000);
-
-//   test('Widget buttons should show popover on click and close popover on next click', async () => {
-//     await page.goto('http://localhost:9000');
-
-//     await page.waitForSelector('.widget');
-
-//     const widget = await page.$('.widget');
-//     const buttons = await widget.$$('.btn');
-
-//     for (let button of buttons) {
-//       await button.click();
-//       await page.waitForSelector('.popover');
-
-//       await button.click();
-//       const popovers = await page.$$('.popover')
-//       expect(popovers.length).toBe(0)      
-//     }
-//   }, 130000);
-
-//   afterEach(async () => {
-//     await browser.close();
-//   });
-// });
+      const buttons = document.querySelectorAll('.btn');
+      
+        for (let button of buttons) {
+          button.click();
+          let popovers = document.querySelectorAll('.popover');
+          expect(popovers.length).toBe(1)
+      
+          button.click();
+          popovers = document.querySelectorAll('.popover');
+          expect(popovers.length).toBe(0)      
+          }
+        });
