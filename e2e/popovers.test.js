e9538bee9e4f142ae8ea5ef0a57a591ff7263d54
Tatiana Yuryeva <yuryevat@list.ru>
2023-06-14 15:11:50 +0200

    appveyor

--- e2e/popovers.test.js
+++ e2e/popovers.test.js
@@ -6,7 +6,7 @@ describe('Popover widget', () => {
 
   beforeEach(async () => {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 100,
       devtools: true,
       args: ["--no-sandbox"]
