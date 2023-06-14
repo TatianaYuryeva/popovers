3f1ec0c8508456f58f74639b250929a7231fa99d
Tatiana Yuryeva <yuryevat@list.ru>
2023-06-14 17:56:42 +0200

    appveyor error fixed

--- src/js/widget.js
+++ src/js/widget.js
@@ -1,7 +1,6 @@
 export class Widget {
   constructor(parentEl) {
     this.parentEl = parentEl;
-    //this.onSubmit = this.onSubmit.bind(this);
   }
 
   static get markup() {
@@ -13,48 +12,7 @@ export class Widget {
       `;
   }
 
-  // static get submitSelector() {
-  //   return ".submit";
-  // }
-
-  // static get inputSelector() {
-  //   return ".input";
-  // }
-
-  // static get selector() {
-  //   return ".card-form";
-  // }
-
   bindToDOM() {
     this.parentEl.innerHTML = Widget.markup;
-    // this.element = this.parentEl.querySelector(CardNumberFormWidget.selector);
-    // this.submit = this.element.querySelector(
-    //   CardNumberFormWidget.submitSelector
-    // );
-    // this.input = this.element.querySelector(CardNumberFormWidget.inputSelector);
-    // this.cards = this.parentEl.querySelectorAll(".card");
-
-    // this.element.addEventListener("submit", this.onSubmit);
   }
-
-  // onSubmit(e) {
-  //   e.preventDefault();
-
-  //   for (const card of this.cards) {
-  //     card.classList.add("disabled");
-  //   }
-
-  //   const value = this.input.value;
-  //   const paySystem = validator.checkPaySystem(value);
-  //   const paySystemIcon = document.querySelector(`.${paySystem}`);
-
-  //   if (validator.isValidCheckDigit(value)) {
-  //     paySystemIcon.classList.remove("disabled");
-  //     this.input.classList.add("valid");
-  //     this.input.classList.remove("invalid");
-  //   } else {
-  //     this.input.classList.add("invalid");
-  //     this.input.classList.remove("valid");
-  //   }
-  // }
 }
